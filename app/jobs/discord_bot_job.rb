b6b0f77f0e1dbe9555f617f2b27f4584612df9f4
ryanboydguam <ryanboydguam@gmail.com>
2022-01-27 23:45:16 +1000

    changed pg version

---
+++ app/jobs/discord_bot_job.rb
@@ -0,0 +1,9 @@
+
+# Dir["C:\Ruby30-x64\lib\ruby\gems\3.0.0\gems\net-ping-2.0.8\lib\net\*.rb"].each {|file| require file }
+# bot = Discordrb::Commands::CommandBot.new token: 'ODcyMTgyMTE3MDg0MzExNjIy.YQmI5Q.-zdY0zNG5XnVgi079e_UwD5CNEU'
+# bot.message(content: '!Setup') do |event|
+#     event.message.delete
+#     dcConstant = event.channel.name
+#     event.respond "test #{dcConstant}"
+# end
+# bot.send_message(933711410230333471,"hello")
