9ec237e369b94ea510569053abe1821a42e859e9
ryanboydguam <ryanboydguam@gmail.com>
2022-02-26 20:35:52 +1000

    updating job for maintance check

--- app/jobs/address_status_check_job.rb
+++ app/jobs/address_status_check_job.rb
@@ -7,11 +7,13 @@ require 'net/ping'
       tmpSites.each do |x| #for each site on the site page
         if x.maintenance?
           checkFloat = upURL(x.address,x.delay)
-          tmpNetworkpage = Networkpage.new#creates a new networkpage object
+          tmpNetworkpage = x.networkpages.build#creates a new networkpage object
           if checkFloat == 5.0
             tmpNetworkpage.status = "Offline"
+            tmpNetworkpage.site.status = "Offline"
           else
             tmpNetworkpage.status = "Online"
+            tmpNetworkpage.site.status = "Online"
           end
           tmpNetworkpage.address = x.address
           tmpNetworkpage.addressindex = x.id
@@ -20,6 +22,7 @@ require 'net/ping'
           tmpNetworkpage.creation = Time.now
           tmpNetworkpage.site = x
           tmpNetworkpage.save
+          
           ActionCable.server.broadcast("sites:#{x.id}",{siteId: x.id,htmlRender: SitesController.render(
             partial: 'sites/site', locals: { site: x }
           ),pingStatus: tmpNetworkpage.status
@@ -27,7 +30,28 @@ require 'net/ping'
           })
           p tmpNetworkpage
           p "[UPDATE]"
+        else
+          
+          # tmpNetworkpage = Networkpage.new#creates a new networkpage object
+          # tmpNetworkpage.status = "Maintenance"
+          # tmpNetworkpage.site.status = "Maintenance"
+          # tmpNetworkpage.address = x.address
+          # tmpNetworkpage.addressindex = x.id
+          # tmpNetworkpage.monitor = 2
+          # tmpNetworkpage.relaytime = 0
+          # tmpNetworkpage.creation = Time.now
+          # tmpNetworkpage.site = x
+          # tmpNetworkpage.save
+          
+          # ActionCable.server.broadcast("sites:#{x.id}",{siteId: x.id,htmlRender: SitesController.render(
+          #   partial: 'sites/site', locals: { site: x }
+          # ),pingStatus: tmpNetworkpage.status
+          
+          # })
+          # p tmpNetworkpage
+          # p "[UPDATE]"
         end
+
       end
     end
     def upURL(host,time)
