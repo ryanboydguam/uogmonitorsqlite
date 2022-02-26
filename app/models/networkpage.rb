c9a4457ab728f08c22ffb163b79508835ae6d1c9
ryanboydguam <ryanboydguam@gmail.com>
2022-02-26 20:36:15 +1000

    added maintenance

--- app/models/networkpage.rb
+++ app/models/networkpage.rb
@@ -1,9 +1,13 @@
 class Networkpage < ApplicationRecord
     belongs_to :site
+    accepts_nested_attributes_for :site #For all saves for networkpages it also saves to sites
     def offline?
         status == "Offline" 
     end
     def online?
         status == "Online" 
+    end
+    def maintenance?
+        status == "Maintenance" 
     end
 end
