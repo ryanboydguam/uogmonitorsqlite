557bb9d1e9fcd540705eeab621240bbf7525e518
ryanboydguam <ryanboydguam@gmail.com>
2022-03-22 15:09:32 +1000

    Changeing site relationships

--- app/models/site.rb
+++ app/models/site.rb
@@ -4,6 +4,7 @@ class Site < ApplicationRecord
     scope :offline,->{ where(status: "Offline")}
     scope :online,->{ where(status: "Online")}
     scope :maintenance,->{ where(maintenance: false) }
+    belongs_to :user
     before_save do |site|#switch for maintances. So if it is false then it will update the status
         unless site.maintenance?
             site.status = "Maintenance"
