5d8b33d6e91eba4a6e581b22fc4b8cf3afdf5baf
ryanboydguam <ryanboydguam@gmail.com>
2022-03-28 09:48:40 +1000

    Jobs now updated mean,lastseen,and houruptime

--- app/jobs/address_status_check_job.rb
+++ app/jobs/address_status_check_job.rb
@@ -19,7 +19,32 @@ class AddressStatusCheckJob < ApplicationJob
       tmpNetworkpage.creation = Time.now
       tmpNetworkpage.site = site
       site.mean=(site.networkpages.order(id: :desc).limit(60).average(:relaytime)).round(4)
-      #site.houruptime=site.networkpages
+      sitestatus=site.networkpages.last(60).pluck(:status).reverse
+      site.lastseen=sitestatus.find_index {|status| status=="Online"}
+      sitestatus_hash=sitestatus.each_with_object(Hash.new(0)){|word,counts| counts[word]+=1}
+
+      site.houruptime=(sitestatus_hash["Online"].to_f/60.to_f)*100
+
+      tmpNetworkpage.save
+
+      ActionCable.server.broadcast("sites:#{site.id}", { siteId: site.id, htmlRender: SitesController.render(
+        partial: 'sites/site', locals: { site: site }
+      ), pingStatus: tmpNetworkpage.status })
+      p tmpNetworkpage
+      p '[UPDATE]'
+    else
+      tmpNetworkpage = site.networkpages.build # creates a new networkpage object
+      tmpNetworkpage.status = false
+      tmpNetworkpage.site.status = false
+      tmpNetworkpage.address = site.address
+      tmpNetworkpage.addressindex = site.id
+      tmpNetworkpage.monitor = 2
+      tmpNetworkpage.relaytime = 0
+      tmpNetworkpage.creation = Time.now
+      tmpNetworkpage.site = site
+      site.mean=0
+      site.lastseen=0
+      site.houruptime=0
       tmpNetworkpage.save
 
       ActionCable.server.broadcast("sites:#{site.id}", { siteId: site.id, htmlRender: SitesController.render(
@@ -28,7 +53,6 @@ class AddressStatusCheckJob < ApplicationJob
       p tmpNetworkpage
       p '[UPDATE]'
     end
-
   end
   def upURL(host, time)
     check = Net::Ping::HTTP.new(host, nil, time)
