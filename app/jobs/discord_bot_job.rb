3e55bae654db8a1080d2d2a59041647989b16ae6
ryanboydguam <ryanboydguam@gmail.com>
2022-04-07 19:40:12 +1000

    updated discord bot

--- app/jobs/discord_bot_job.rb
+++ app/jobs/discord_bot_job.rb
@@ -3,35 +3,31 @@ class DiscordBotJob < ApplicationJob
     require 'net/ping'
     queue_as :default
     def perform(*args)
-        puts "hello"
         Dir["C:\Ruby30-x64\lib\ruby\gems\3.0.0\gems\net-ping-2.0.8\lib\net\*.rb"].each {|file| require file }
-        bot = Discordrb::Commands::CommandBot.new token: 'ODcyMTgyMTE3MDg0MzExNjIy.YQmI5Q.wjgafluEvZ4T8Hl_kjhuks5OQa4'
+        bot = Discordrb::Commands::CommandBot.new token: 'Njc4MjYyMjY5NjQwOTY2MTk0.XkgO9g.RzSJg9YBC6_kS2s4FzyIuz5aGok'
         # bot.message(content: '!Setup') do |event|
         #     event.message.delete
         #     dcConstant = event.channel.name
         #     event.respond "test #{dcConstant}"
         # end
-        bot.send_message(933711410230333471,"UOG MONITOR SYSTEM #{Time.now}")
+        channel_id=933711410230333471
+        if time.min >0 &&time.min < 2
+        bot.send_message(channel_id,"UOG MONITOR SYSTEM :ONLINE")
+            @sites.each_with_index.map do |site,i|
+                bot.send_message(channel_id,"#{site.address} IS CURRENTLY #{site.status}")
+                sleep(1)
+            end
+        end
         @sites = Site.all
         @sites.each_with_index.map do |site,i|
-            # site_data_array = site.networkpages.last(2).pluck(:relaytime).reverse()
-            array_status=site.networkpages.last(60).pluck(:status).reverse()
-            last_seen_counter=0
-            array_status_result=array_status.each_with_object(Hash.new(0)){|word,counts| counts[word]+=1}
-            if array_status_result["Offline"] > 0
-                array_status.each do |value|
-                    last_seen_counter+=1
-                    if value == "Offline"
-                        break
-                    end
-                end
+            if site.houruptime < 90 && site.status == "Offline"
+                bot.send_message(channel_id,"#{site.address} IS CURRENTLY #{site.status}")
+                bot.send_message(channel_id,"#{site.address} Was last seen #{site.lastseen} Minutes Ago")
             end
-            output=(array_status_result["Online"].to_f/(array_status_result["Online"].to_f+array_status_result["Offline"].to_f)*100).to_i
-
-        bot.send_message(933711410230333471,"```#{site.address} UpTime: #{output} % Last Seen: #{last_seen_counter}```")
+            if site.status == "Offline"
+                bot.send_message(channel_id,"#{site.address} IS CURRENTLY #{site.status}")
+                bot.send_message(channel_id,"THIS DOES NOT MEAN IT IS DOWN")
+            end
         end
-
-
-        bot.send_message(933711410230333471,"Test Data")
     end
 end
